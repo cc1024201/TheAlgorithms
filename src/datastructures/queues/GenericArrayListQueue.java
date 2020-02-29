4e72006b0ce00fd19567f931035c4e94f2f28ab6
apollo <cc931559391@gmail.com>
2020-02-29 17:29:39 +0800

    queues

--- src/datastructures/queues/GenericArrayListQueue.java
+++ src/datastructures/queues/GenericArrayListQueue.java
@@ -11,22 +11,26 @@ import java.util.ArrayList;
  */
 public class GenericArrayListQueue<T> {
     ArrayList<T> _queue = new ArrayList<>();
-    private boolean hasElements(){
+
+    private boolean hasElements() {
         return !_queue.isEmpty();
     }
-    public T peek(){
+
+    public T peek() {
         T result = null;
-        if(this.hasElements()){
+        if (this.hasElements()) {
             result = _queue.get(0);
         }
         return result;
     }
-    public boolean add(T element){
+
+    public boolean add(T element) {
         return _queue.add(element);
     }
-    public T poll(){
+
+    public T poll() {
         T result = null;
-        if(this.hasElements()){
+        if (this.hasElements()) {
             result = _queue.remove(0);
         }
         return result;
